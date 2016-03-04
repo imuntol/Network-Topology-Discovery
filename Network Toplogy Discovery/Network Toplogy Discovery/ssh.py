ac986045174af30ba2e3a63428621ccde7206781
Fone <vutiphan1016@hotmail.com>
2016-03-04 17:33:54 +0700

    use netmiko to connect ssh

--- Network Toplogy Discovery/Network Toplogy Discovery/ssh.py
+++ Network Toplogy Discovery/Network Toplogy Discovery/ssh.py
@@ -1,13 +1,43 @@
-import paramiko
+﻿import paramiko
+from netmiko import ConnectHandler
 
-ip = "192.168.250.3"
+ip = "192.168.1.2"
 username = "admin"
 password = "admin"
 
-ssh = paramiko.SSHClient()
-ssh.load_system_host_keys()
-ssh.connect(ip, username = "admin", password = "admin")
-#ssh.send("terminal length 0"+"\n")
-#ssh.send("sh spanning-tree detail | include Port"+"\n")
-#output = ssh.recv(5000)
-#print output
+#cisco_881 = {
+#'device_type': 'cisco_ios',
+#'ip': ip,
+#'username': username,
+#'password': password,
+#} 
+
+net_connect = ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
+output = net_connect.send_command("sh ip int br")
+print output
+
+#temp = []
+#data = []
+#temp.append(output)
+##print data
+#temp[0] = temp[0].split("\r\n")
+##print data[0]
+
+#for i in temp[0]:
+#    if "(" in i:
+#        data.append(i)
+##print data
+#for i in range(0,len(data)):
+#     data[i] = data[i].split("(")[1];
+#     data[i] = data[i].split(")");
+##print data
+##print data[0][0]
+#for j in range(0,len(data)):
+#    data[j][1] = data[j][1].split("is ")[1];
+#    data[j][1] = data[j][1].split(" ")[0];
+#print data
+
+
+##ssh = paramiko.SSHClient()
+##ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+###ssh.connect(ip, username=username, password=password)
